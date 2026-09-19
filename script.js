fc70902d793f44093329e20b29a5b25bf164996d
PolkaMC <114118832+PolkaMC@users.noreply.github.com>
2026-09-18 23:14:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+(function shuffle() {
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+    });
+})();
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+function flipCard() {
+    if(lockBoard) return;
+    this.classList.add('flip');
+
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if(firstCard.dataset.itgenik === secondCard.dataset.itgenik) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+ 
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip')
+
+        resetBoard();
+    }, 1500);
+}
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard))
